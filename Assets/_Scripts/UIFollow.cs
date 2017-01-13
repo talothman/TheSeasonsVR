12477126ab0853844c5c887fbc4e42600348f5ae
Talal Alothman <stenfeio@gmail.com>
2017-01-13 14:49:23 -0500

    Initial commet

---
+++ Assets/_Scripts/UIFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIFollow : MonoBehaviour {
+    public GameObject objectToFollow;
+    public GameObject objectToLookAt;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = objectToFollow.transform.position;
+        //transform.LookAt(objectToLookAt.transform);
+	}
+}
