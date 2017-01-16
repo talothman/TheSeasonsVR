6809f4caab42bbea7951771a108190eda18ee45a
Talal Alothman <stenfeio@gmail.com>
2017-01-16 11:43:45 -0500

    added axis to center of earth.

---
+++ Assets/CenterOfEarth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Vectrosity;
+
+public class CenterOfEarth : MonoBehaviour {
+    VectorLine lineThroughEarth;
+    public Transform northPole;
+    public Transform southPole;
+	
+    // Use this for initialization
+	void Start () {
+	    lineThroughEarth = VectorLine.SetLine3D(Color.white, northPole.position, southPole.position);
+        lineThroughEarth.lineWidth = 1.5f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        lineThroughEarth.points3[0] = northPole.position;
+        lineThroughEarth.points3[1] = southPole.position;
+    }
+}
