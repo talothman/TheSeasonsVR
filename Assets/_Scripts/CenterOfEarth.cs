ec3e3e8918ecf4ed42ef8a4c3df8569acb1c27c8
Talal Alothman <stenfeio@gmail.com>
2017-01-19 14:51:25 -0500

    changed scale and added DOTween

--- Assets/_Scripts/CenterOfEarth.cs
+++ Assets/_Scripts/CenterOfEarth.cs
@@ -11,11 +11,13 @@ public class CenterOfEarth : MonoBehaviour {
     // Use this for initialization
 	void Start () {
 	    lineThroughEarth = VectorLine.SetLine3D(Color.white, northPole.position, southPole.position);
-        lineThroughEarth.lineWidth = 1.5f;
+        lineThroughEarth.lineWidth = 0.009f;
+        VectorLine.SetCamera3D(GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>());
     }
 	
 	// Update is called once per frame
 	void Update () {
+        
         lineThroughEarth.points3[0] = northPole.position;
         lineThroughEarth.points3[1] = southPole.position;
     }
--- Assets/_Scripts/UIFollow.cs
+++ Assets/_Scripts/UIFollow.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class UIFollow : MonoBehaviour {
     public GameObject objectToFollow;
     public GameObject objectToLookAt;
+
+    public Vector3 offset = Vector3.zero;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -11,7 +14,9 @@ public class UIFollow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = objectToFollow.transform.position;
-        //transform.LookAt(objectToLookAt.transform);
+        transform.position = objectToFollow.transform.position + offset;
+
+        if (objectToLookAt != null)
+            transform.LookAt(objectToLookAt.transform);
 	}
 }
